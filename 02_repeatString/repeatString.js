34ae8a6cdd167519f7ececebd9fa5f22d3a60e95
Joaquin Sateler <86966872+cacosat@users.noreply.github.com>
2023-09-24 23:05:39 -0300

    solved javascript-excercises 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, reps) {
+    let resultString = "";
+    for (i = 1; i <= reps; i++) {
+        resultString += string;
+    }
+    return resultString;
 };
 
 // Do not edit below this line
