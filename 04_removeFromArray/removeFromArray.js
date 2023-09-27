770afd9db2654aca825fca4d77166c2965dda5ac
Joaquin Sateler <86966872+cacosat@users.noreply.github.com>
2023-09-26 22:43:17 -0300

    04 solved

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...removeElement) {  // "...removeElement" is a rest parameter
+    for (let i = 0; i <= array.length; i++) {
+        for (let j = 0; j <= removeElement.length; j++) {
+            if (removeElement.sort()[j] === array[i]) {
+                //array sorted to avoid array parameter index changing problems
+                array.splice(i, 1);
+            }
+        }
+    }
+    return array
 };
 
 // Do not edit below this line
