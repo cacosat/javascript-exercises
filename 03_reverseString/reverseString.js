651c78143090e48cc4aab627508cb82c3f0f53f0
Joaquin Sateler <86966872+cacosat@users.noreply.github.com>
2023-09-26 21:56:12 -0300

    03 solved

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let chars = string.split("");
+    let reverseString = [];
+    for (let i = 0; i < string.length; i++) {
+        reverseString += chars.pop(); //pop is an array method
+    }
+    return reverseString;
 };
 
 // Do not edit below this line
