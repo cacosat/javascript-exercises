05b6c01bdf61324f9f93aa352d78ef9de752ecc9
Joaquin Sateler <86966872+cacosat@users.noreply.github.com>
2023-09-28 22:58:54 -0300

    08 solved

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,13 @@
 const convertToCelsius = function(f) {
   // °C = 5/9(°F – 32)
   let c = 5/9 * (f - 32);
-  return Number((c).toFixed(1))
+  return Number((c).toFixed(1)) // method gives string, then is converted to num
 };
 
 const convertToFahrenheit = function(c) {
   // °F = (°C * 9/5) + 32
   let f = (c * 9/5) + 32
-  return Number((f).toFixed(1))
+  return Number((f).toFixed(1)) // method gives string, then is converted to num
 };
 
 // Do not edit below this line
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a, b) {
+	let add = a + b;
+  return add
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	let subtract = a - b;
+  return subtract
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  let sum = 0;
+  for (let i = 0; i < a.length; i++) {
+    sum += a[i];
+  }
+	return sum
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  let multiply = 1;
+  for (let i = 0; i < a.length; i++) {
+    multiply *= a[i];
+  }
+  return multiply
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let pwr = a**b
+	return pwr
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let factorial = 1;
+  for (let i = 1; i <= a; i++) {
+    factorial *= i;
+  }
+	return factorial
 };
 
 // Do not edit below this line
