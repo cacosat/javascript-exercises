87c9fabf53a506046c9ce157973e3d9e9936675f
Joaquin Sateler <86966872+cacosat@users.noreply.github.com>
2023-10-01 16:06:46 -0300

    11 solved

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,13 @@
-const getTheTitles = function() {
+const getTheTitles = function(array) {
+// takes an input array of books (obj) and returns array of titles (property)
+// 1. Access title property from elements (books) of input array
+// 2. Add property value to new resultArray of titles
 
+let titles = [];
+for (let i = 0; i < array.length; i++) {
+    titles.push(array[i]["title"]); //could be array[i].title;
+}
+return titles;
 };
 
 // Do not edit below this line
