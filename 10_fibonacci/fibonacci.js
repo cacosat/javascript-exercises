db59f76afa3cf77134f84841e9bf87f6ee67b229
Joaquin Sateler <86966872+cacosat@users.noreply.github.com>
2023-10-01 13:30:03 -0300

    10 solved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let sequence = [1, 1];
+    if (num < 0) {
+        return 'OOPS'
+    } else {
+        for (let i = 0; i < num; i++) {
+            sequence.push(sequence[i]+sequence[i+1]);
+        }
+        return sequence[num-1]
+    }
 };
 
 // Do not edit below this line
